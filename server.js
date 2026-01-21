2a53d6cacb9aceac3cb4a79134f0da086cd3b112
ganesh-more2006 <moreganesh855197@gmail.com>
2026-01-21 13:01:23 +0530

    Final CORS fix and port 8080 update

--- server.js
+++ server.js
@@ -9,14 +9,13 @@ dotenv.config();
 connectDB(); 
 
 const app = express();
-// const cors = require('cors');
-// app.use(cors({
-//   origin: true,
-//   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-//   allowedHeaders: ["Content-Type", "Authorization"],
-//   credentials: true
-// }));
-app.use(cors());
+
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
+}));;
 app.use(express.json()); 
 
 app.use('/api/products', productRoutes);

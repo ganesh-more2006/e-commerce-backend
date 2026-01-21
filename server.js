d343e57d85c964d80f1469384ba82156dc8953b8
ganesh-more2006 <moreganesh855197@gmail.com>
2026-01-21 12:37:38 +0530

    Fixed CORS origin and added localhost 127.0.0.1 for local testing

--- server.js
+++ server.js
@@ -10,8 +10,16 @@ connectDB();
 
 const app = express();
 
-
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://ganesh-shop-frontend.vercel.app" 
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+// app.use(cors());
 app.use(express.json()); 
 
 app.use('/api/products', productRoutes);
